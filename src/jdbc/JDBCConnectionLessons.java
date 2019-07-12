8d96198811da389e8158f73dab3dfb2ac542d2fa
Serega <kamserg1608@gmail.com>
2019-07-12 12:23:33 +0300

    Lesson 188

--- src/jdbc/JDBCConnectionLessons.java
+++ src/jdbc/JDBCConnectionLessons.java
@@ -2,9 +2,7 @@ package jdbc;
 
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static java.lang.Class.forName;
 
@@ -12,10 +10,31 @@ public class JDBCConnectionLessons {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         String userName = "root";
         String password = "root";
-        String connectionURl = "jdbc:mysql://localhost:3306/";
+        String connectionURl = "jdbc:mysql://localhost:3306/test";
         forName("com.mysql.jdbc.Driver");
-        try(Connection connection = DriverManager.getConnection(connectionURl, userName, password)){
-            System.out.println("We're connected!!!!");
+        try(Connection connection = DriverManager.getConnection(connectionURl, userName, password);
+            Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE Books");
+            statement.executeUpdate("CREATE TABLE Books (id SERIAL, name VARCHAR(30) NOT NULL , PRIMARY KEY (id) )");
+            statement.executeUpdate("INSERT INTO Books (name) VALUES('Inferno')");
+            statement.executeUpdate("INSERT INTO Books (name) VALUES('Solomon key')");
+            ResultSet resultSet = statement.executeQuery("SELECT  * FROM Books");
+
+            while (resultSet.next()){
+//                System.out.println(resultSet.getInt(1));
+                System.out.println(resultSet.getInt("id"));
+//                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString("name"));
+
+                System.out.println("-------------------");
+            }
+
+            System.out.println("__________________________");
+
+            ResultSet rs2 = statement.executeQuery("SELECT  name FROM books WHERE id = 1");
+            while(rs2.next()) {
+                System.out.println(rs2.getString(1));
+            }
         }
     }
 }
